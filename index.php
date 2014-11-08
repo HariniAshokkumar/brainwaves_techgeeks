dcedd47c423ee37f16947d9f4683f34e0f45abdc
kavyakp <kaavyaa.93@gmail.com>
2014-11-08 12:12:53 +0530

    My first commit

--- index.php
+++ index.php
@@ -17,11 +17,18 @@
     <![endif]-->
   </head>
   <body>
-    <h1>UBIQSERVE</h1>
+    <h1 align="center">UBIQSERVE</h1>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-      
+      <form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+      <h1>Welcome to UBIQSERVE service</h1>
+      <h4>Please enter your account number</h4><br>
+    <input type="text" class="form-control"><br>
+  </div><br>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
   </body>
 </html>
